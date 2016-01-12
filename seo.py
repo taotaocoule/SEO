aef779abf32875337e54e4c09e50fa610fc9d8f9
marius <2443877012@qq.com>
2016-01-12 17:05:57 +0800

    data from excelhome

---
+++ seo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-  
+
+# seo data
+import urllib
+import urllib2
+from lxml import *
+import lxml.html as HTML 
+
+KeyUrl='http://club.excelhome.net/'
+FirstPage='http://club.excelhome.net/forum.php'
+
+PageDownload=urllib2.urlopen(FirstPage)
+PageRead=PageDownload.read()
+
+PageInHtml=HTML.document_fromstring(PageRead)
+KewordXpath=r"//div[@id='portal_block_19_content']/div/div/a"
+KeywordUrlXpath=r"//div[@id='portal_block_19_content']/div/div/a/@href"
+
+Keyword=PageInHtml.xpath(KewordXpath)
+KeywordUrl=PageInHtml.xpath(KeywordUrlXpath)
+for i in range(len(Keyword)):
+	url="http://club.excelhome.net/" + KeywordUrl[i].strip()
+	print Keyword[i].text,url
+	
